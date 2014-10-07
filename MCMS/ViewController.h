7976ede9500a1098ed11b5735580453d0cbc1410
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-07 14:37:12 -0500

    task #2 and #3 complete

--- MCMS/ViewController.h
+++ MCMS/ViewController.h
@@ -9,6 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITableView *myTableView;
+@property (strong, nonatomic) IBOutlet UITextField *creatureTextField;
 
 
 @end
--- MCMS/ViewController.m
+++ MCMS/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "MagicalCreature.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
 @property NSMutableArray *creatures;
 @end
 
@@ -21,15 +21,42 @@
     pegasusCreature.name = @"Pegasus";
 
     MagicalCreature *trollCreature = [[MagicalCreature alloc]init];
-    trollCreature.name = @"Troll";
+    trollCreature.name = @"troll";
 
     MagicalCreature *cerberusCreature = [[MagicalCreature alloc]init];
     cerberusCreature.name = @"Cerberus";
+
+    self.creatures = [[NSMutableArray alloc] initWithObjects:pegasusCreature, trollCreature, cerberusCreature, nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.creatures.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"creatureCellID" forIndexPath:indexPath];
+
+    MagicalCreature *creature = [self.creatures objectAtIndex:indexPath.row];
+    cell.textLabel.text = creature.name;
+
+    return cell;
+}
+- (IBAction)onAddButtonPressed:(id)sender {
+    if (![self.creatureTextField.text isEqualToString:@""]) {
+        MagicalCreature *creature = [[MagicalCreature alloc] init];
+        creature.name = [self.creatureTextField text];
+
+        [self.creatures addObject:creature];
+
+        self.creatureTextField.text = @"";
+        [self.myTableView reloadData];
+    }
+}
+
 
 @end
